7624b41b2998b9e4f758054c3a6f1b167c8f7172
Gabriel Coelho <gabrielcoelhocn08@gmail.com>
2024-05-29 14:49:32 -0300

    created db and models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+broz = Restaurant.create(name: 'Broz Pizzaria', address: 'Rua Monte Santo',
+                         phone_number: '21 98764 6746',
+                         category: 'italian')
+
+la_tratoria = Restaurant.create(name: 'La Tratoria', address: 'Rua aloe Santo',
+                         phone_number: '21 98454 6746',
+                         category: 'french')
+
+shein = Restaurant.create(name: 'Shein', address: 'Rua tavares',
+                         phone_number: '21 92264 6746',
+                         category: 'chinese')
+
+gurume = Restaurant.create(name: 'Gurume', address: 'Rua barao da torre',
+                         phone_number: '21 98234 6746',
+                         category: 'japonese')
+
+tomorrowland = Restaurant.create(name: 'Tomorrowland', address: 'Rua do paraiso',
+                          phone_number: '21 39294 6746',
+                          category: 'belgian')
